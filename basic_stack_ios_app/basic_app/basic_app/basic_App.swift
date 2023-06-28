6bc5dc9ba9d65639e758e0e6eb0602241f0651f6
sharansudheer <saran_sudheer@hotmail.com>
2023-06-28 23:31:24 +0530

    First PR

---
+++ basic_stack_ios_app/basic_app/basic_app/basic_App.swift
@@ -0,0 +1,17 @@
+//
+//  basic_appApp.swift
+//  basic_app
+//
+//  Created by Sharan Sudheer on 11/06/23.
+//
+
+import SwiftUI
+
+@main
+struct basic_App: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
